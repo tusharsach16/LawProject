ad4284740752acb9faa37b942305643873b1dc6a
Tushar <tusharschdeva16@gmail.com>
2025-09-13 01:15:15 +0530

    Added public and private routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,35 @@
-import Signin from "./pages/Signin"
-import SignUp from "./pages/SignUp"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PublicLayout from "./layouts/PublicLayout";
 import Dashboard1 from "./pages/Dashboard1";
 import Home from './pages/Home';
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import ConnectWithLawyers from "./pages/ConnectWithLawyers"; 
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-
   return (
-    <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/signin" element={<Signin/>}/>
-        <Route path="/signUp" element={<SignUp/>}/>
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/talk-to-lawyer" element={<ConnectWithLawyers />} />
+        </Route>
+
+        {/* PRIVATE/DASHBOARD ROUTES (Sidebar ke saath)*/}
         <Route 
           path="/dashboard1/*" 
           element={
             <ProtectedRoute>
               <Dashboard1 />
             </ProtectedRoute>
-          }
+          } 
         />
       </Routes>
     </BrowserRouter>
-    </>    
-  )
+  );
 }
 
-export default App
+export default App;
