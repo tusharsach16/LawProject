da7fe3eac3161206cce6768a421dc010aa7c1f7b
Tushar <tusharschdeva16@gmail.com>
2025-09-13 01:14:41 +0530

    Remove Header from here as we already added Header in PublicLayout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import Featuresection from "../components/Featuresection"
 import Footer from "../components/Footer"
 import GetHelp from "../components/GetHelp"
-import Headers from "../components/Headers"
 import Herosection from "../components/Herosection"
 import Stats from "../components/Stats"
 const Home = () => {
   return (
     <>
-      <Headers/>
       <Herosection/>
       <Featuresection/>
       <Stats/>
