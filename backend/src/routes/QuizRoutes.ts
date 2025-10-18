cf5ac389bf6e1f2e0ec3472af337e24798b01594
Tushar <tusharschdeva16@gmail.com>
2025-10-19 02:19:00 +0530

    Build routes for quiz results and count of quiz and recent activity

--- backend/src/routes/QuizRoutes.ts
+++ backend/src/routes/QuizRoutes.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from 'express';
-import  {getQuizQues, result, submitAttempt}  from '../controllers/quizController';
+import  {getQuizQues, result, submitAttempt, getQuizCount, getDetailedQuizResults, getRecentActivities, getQuizStatistics}  from '../controllers/quizController';
 import authMiddleware from '../middleware/authMiddleware';
 const router = express.Router();
 
-router.get('/getQuiz', authMiddleware, getQuizQues);
-router.post('/submit', authMiddleware, submitAttempt);
-router.get('/getResult', authMiddleware, result);
+router.get('/quiz/getQuiz', authMiddleware, getQuizQues);
+router.post('/quiz/submit', authMiddleware, submitAttempt);
+router.get('/quiz/getResult', authMiddleware, result);
+router.get('/quiz/count', authMiddleware, getQuizCount);
+router.get('/quiz/detailed-results', authMiddleware, getDetailedQuizResults);
+router.get('/quiz/statistics', authMiddleware, getQuizStatistics);
+router.get('/quiz/recent-activities', authMiddleware, getRecentActivities);
 export default router;
