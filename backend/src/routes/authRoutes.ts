5c9631b976318bd8b2451e7088bc42427441f93f
Tushar <tusharschdeva16@gmail.com>
2025-11-08 03:06:13 +0530

    Add routed foor reseting pass

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { signupUser, loginUser, getUser } from '../controllers/authController';
 import authMiddleware from '../middleware/authMiddleware';
+import { forgotPassword, resetPassword, verifyOtp } from '../controllers/forgotPassword';
 
 const router = express.Router();
 
@@ -8,4 +9,9 @@ router.post('/signup', signupUser);
 router.post('/login', loginUser);
 
 router.get('/get', authMiddleware, getUser);
+
+router.post('/forgot-password', forgotPassword);
+router.post('/verify-otp', verifyOtp);
+router.post('/reset-password', resetPassword);
+
 export default router;
