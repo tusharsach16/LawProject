1e0b2587c0aea0690e36c674a5cbd903844d3b75
Tushar <tusharschdeva16@gmail.com>
2025-11-08 22:55:23 +0530

    Fixed origin website link

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://nyaysetu.vercel.app"
+    "https://nyaysetu-psi.vercel.app"
   ],
   credentials: true, // only if you're sending cookies or authorization headers
 }));
