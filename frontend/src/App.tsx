28c8e12f53f8d8b560032e49fc154b6f2820fbc0
Tushar <tusharschdeva16@gmail.com>
2025-12-25 00:59:12 +0530

    Added route path for condition pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,11 @@ import ConnectWithLawyers from "./pages/ConnectWithLawyers";
 import ProtectedRoute from "./components/ProtectedRoute";
 import MockTrialLobby from "./pages/MockTrialLobby";
 import ForgotPasswordFlow from "./pages/ForgotPassword";
+import LawyerDashboard from "./pages/LawyerDashboard";
+import TermsAndConditions from './pages/legal/TermsAndConditions';
+import PrivacyPolicy from './pages/legal/PrivacyPolicy';
+import RefundPolicy from './pages/legal/RefundPolicy';
+
 
 function App() {
   return (
@@ -22,6 +27,9 @@ function App() {
           <Route path="/forgotpassword" element={<ForgotPasswordFlow/>} />
         </Route>
 
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/refund-policy" element={<RefundPolicy />} />
         {/* PRIVATE/DASHBOARD ROUTES (Sidebar ke saath)*/}
         <Route 
           path="/dashboard/*" 
@@ -30,6 +38,15 @@ function App() {
               <Dashboard />
             </ProtectedRoute>
           } 
+        />
+
+        <Route
+          path="/lawyer-dashboard/*"
+          element={
+            <ProtectedRoute>
+              <LawyerDashboard />
+            </ProtectedRoute>
+          }
         />
       </Routes>
     </BrowserRouter>
