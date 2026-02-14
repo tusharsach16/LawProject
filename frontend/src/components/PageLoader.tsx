1c5777edea1da9820bb930730eb4606f0b375929
Tushar <tusharschdeva16@gmail.com>
2026-02-15 02:24:08 +0530

    Create component pageloader

---
+++ frontend/src/components/PageLoader.tsx
@@ -0,0 +1,9 @@
+const PageLoader = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900" />
+        </div>
+    );
+};
+
+export default PageLoader;
