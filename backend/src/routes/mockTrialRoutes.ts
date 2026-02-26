5a6b52125cb4eb36a89e5052f7df5899ccc0d7e8
Tushar <tusharschdeva16@gmail.com>
2026-02-27 02:12:59 +0530

    Updated mock trial routes

--- backend/src/routes/mockTrialRoutes.ts
+++ backend/src/routes/mockTrialRoutes.ts
@@ -1,15 +1,15 @@
 import express from 'express';
-import { 
-    getMockTrialCategories, 
-    getSituationsCat, 
-    leaveMockTrial, 
-    endMockTrial, 
-    postMockJoin, 
-    postMockMessage, 
-    getMockTrialById, 
-    checkMatchStatus, 
-    analyzeTrialResult, 
-    getPastTrials, 
+import {
+    getMockTrialCategories,
+    getSituationsCat,
+    leaveMockTrial,
+    endMockTrial,
+    postMockJoin,
+    postMockMessage,
+    getMockTrialById,
+    checkMatchStatus,
+    analyzeTrialResult,
+    getPastTrials,
     getMockTrialStatistics
 } from '../controllers/MockTrails/MockTrailController';
 import authMiddleware from '../middleware/authMiddleware';
@@ -19,6 +19,7 @@ const router = express.Router();
 router.get('/mock-trials/situationsCategory', getSituationsCat);
 router.get('/mock-trials/categories', getMockTrialCategories);
 router.get('/mock-trials/past-trials', authMiddleware, getPastTrials);
+router.get('/mock-trials/statistics', authMiddleware, getMockTrialStatistics);
 
 router.get('/mock-trials/check-match/:situationId', authMiddleware, checkMatchStatus);
 router.get('/mock-trials/:trialId', authMiddleware, getMockTrialById);
@@ -29,6 +30,5 @@ router.post('/mock-trials/end', authMiddleware, endMockTrial);
 router.post('/mock-trials/leave', authMiddleware, leaveMockTrial);
 router.post('/mock-trials/message', authMiddleware, postMockMessage);
 router.post('/mock-trials/:trialId/analyse', authMiddleware, analyzeTrialResult);
-router.get('/mock-trials/statistics', authMiddleware, getMockTrialStatistics);
 
 export default router;
