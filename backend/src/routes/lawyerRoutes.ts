91e447906bb0ed73e8f73f36995951e2f683febe
Tushar <tusharschdeva16@gmail.com>
2026-03-13 00:34:32 +0530

    docs: annotate lawyer discovery routes

--- backend/src/routes/lawyerRoutes.ts
+++ backend/src/routes/lawyerRoutes.ts
@@ -2,7 +2,41 @@ import express from "express";
 import { getAllLawyers, getSpecializations } from "../controllers/lawyerController";
 const router = express.Router();
 
+/**
+ * @openapi
+ * tags:
+ *   name: Lawyers
+ *   description: Lawyer profile discovery and filtering
+ */
+
+/**
+ * @openapi
+ * /api/getLawyers:
+ *   get:
+ *     tags: [Lawyers]
+ *     summary: Retrieve all lawyers with optional filtering
+ *     responses:
+ *       200:
+ *         description: List of lawyers
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
 router.get('/getLawyers', getAllLawyers);
+
+/**
+ * @openapi
+ * /api/lawyers/specializations:
+ *   get:
+ *     tags: [Lawyers]
+ *     summary: Get unique list of lawyer specializations
+ *     responses:
+ *       200:
+ *         description: Array of specialization strings
+ */
 router.get('/lawyers/specializations', getSpecializations);
 
 export default router;
